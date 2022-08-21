1fd752310baa40281ebfd30913f5bd7234d9087c
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-21 08:08:11 +0530

    Created and modified tour.php file

---
+++ src/tour.php
@@ -0,0 +1,170 @@
+<html>
+<head>
+<title>Tours</title>
+
+<!---Linking CSS--->
+
+<link rel="stylesheet" href="styles/styles.css">
+<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!---Linking Javascript--->
+
+<script src="js/script.js"></script>
+
+
+</head>
+<body>
+<!---Navigation Bar--->
+<ul class ='menu'>
+	  <li class ='menu'><a href="Homepage.php">Home</a></li>
+	  <li class ='dropdown'><a href="tour.php" class="dropbtn">Wildlife Tours</a>
+			<div class="dropdown-content">
+			  <a href="Leopard safari.php">Leopard safari</a>
+			  <a href="Elephant safari.php">Elephant safari</a>
+			  <a href="Photography tour.php">Photography tour</a>
+			  <a href="Bird watching tour.php">Bird watching tour</a>
+			</div></li>
+	  <li class ='menu'><a href="accommodation.php">Accommodation</a></li>
+	  <li class ='menu'><a href="about us.php">About us</a></li>
+	  <li class ='menu'><a href="contact us.php">Contact us</a></li>
+	</ul>
+	<center><img src = "images/logo.png" style="width:22%"></center>
+	
+<!--slide show -->
+
+<div class="slideshow-container">
+	
+	
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade" style="display: block;">
+    <img src=" images/homeleopard.png" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="images/safari.png" style="width:100%">
+  </div>
+
+  <div class="mySlides fade">
+    <img src="images/peacock.png" style="width:100%">
+  </div>
+  
+ <div class="mySlides fade">
+    <img src="images/bear.png" style="width:100%">
+  </div>
+  
+  <div class="mySlides fade">
+    <img src="images/dear.png" style="width:100%">
+  </div>
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+   <span class="dot" onclick="currentSlide(4)"></span>
+   <span class="dot" onclick="currentSlide(5)"></span>
+</div>
+
+<hr>
+<!--margin-->
+<div style ='margin: 20px 50px 100px 50px;'> 
+
+<!---Wild life safari images and details--->
+
+
+<h1 class="h1"><center>WILD LIFE TOURS</center></h1>
+
+<p class="p3">Leopard safari team are pioneers in wild life safari, experimental travel and hospitality in Sri Lanka.
+We try our best to please our customers by understanding their requirements as much as we can inorder to provide them a 
+unforgettable experience.We provide tented safari camp concept and bush walk camp concept. Without limiting to jeep safari
+we have various other safari activities such as bush walks, canoeing, night drives and walks, meeting the field experts etc.
+since it is our responsibility to given you the best wild life safari tour which not provided by any other we have selected
+some locations which is not much visited by many other troups.As a result all nature lovers who wish to see and experience
+the beauty of mother nature can get it in its max.<br></p>
+
+
+<hr>
+<a href="Leopard safari.php"><h2 class="h1" align="right">Leopard safari </h2></a>
+<hr>
+<img src ="images/Leopard.jpeg" height = "25% " , width ="25%" , align = "right" alt="Leopard image" >
+<p class="p3" align ="left">Leopards are some kind of magnificant carnivourous animals which has a unique behaviour which attracts lot of tourist.
+These creatures can be observed in each an every forest in Sri Lanka because they can adapt to the background easily.According to the 
+statistic Yala national park is considered to be the home for highest number of leopards even from the world leopard population.Yala national park is the ideal 
+place for you and your loved once to appreciate the Sri lankan leopard,Which is also known as "kottiya" in the native language is active during early morning 
+or late evening.It is adviced to visit these areas with a guide since these creatures run away from human attractions.<br><br><br><br><br>
+</p>
+ <hr>
+ <a href="Elephant safari.php"><h2 class="h1" align="Left">Elephant safari </h2></a>
+ <hr>
+ <img src ="images/Elephant.jpeg" height = "25% " , width ="25%" , align = "left" alt="Elephant image" >
+
+ <p class="p3" align="center-right">Elephants in Sri Lanka are the largest animals that can be found in the island. They are one of the enriching spices in the world and here in 
+Sri Lanka we have national parks which are soley dedicated for elephants.Elephants in Sri Lanka are considered to be in a risk of being endengered because 
+of the conflict between humans and elephants.Elephant safari is available in many places it is up to you to select the location. but we would like to recommend Udawalawe 
+national park the most.</p><br><br><br><br><br>
+<hr>
+<a href="Photography tour.php"><h2 class="h1" align="right">Photography tour </h2></a>
+<hr>
+<img src ="images/photography.jpeg" height = "25% " , width ="25%" , align = "right" alt="Bird image" >
+<p class="p3" align="left"></p>
+<br>
+Sri Lanka is one of the coolest country in world which has lot of natural beauty, It is an island which is rich in beautiful water falls, rivers , mountains, trees, birds and 
+wild animals. If your aim is to capture that award winning photograph, why dont you just fix a date and join us for five or more days. The time you spend crawling on ground, 
+that waiting you wait with lot of patience will never be a waste.
+</p><br><br><br><br><br>
+<hr>
+<a href="Bird watching tour.php"><h2 class="h1" align="left">Bird watching tour </h2></a>
+<hr>
+<img src ="images/Bird.jpeg" height = "25% " , width ="25%" , align = "left" alt="Bird image" >
+<p class="p3" align="left"></p>
+<br>
+ We Leopard safari team hire committed, professional naturalists to conduct our birding tours, we offer our clients more than
+ a service. We share our passion for birdwatching in Sri Lanka in particular and nature in general with everyone who enlists our services. Our leaders are very familiar with all
+ the bird watching locations in Sri Lanka and can help you tick as many species off your bird list.
+
+We have more than 35 years’ experience as a birding tour operator in Sri Lanka and our team is led by one of the pioneers in birding tour operations in the country. This is why
+ Adventure Birding is the leading ground agent for European and American birding tour operators which visit Sri Lanka. We are the agent of choice for 13 British tour operators,
+ 2 American tour operators and many others in Sweden, Germany, Ecuador, South Africa, Russia, India, China and Hong Kong along with many birders around the world who want customized
+ bird watching tours in Sri Lanka.
+</p>
+</div>
+<!--Footer-->
+
+	<footer class="social-footer">
+		<div class="social-footer-left">
+		<div style ='text-align:left;margin: 30px 30px 30px 60px ;'>
+		<img src="images/logo.png" style="width:30%"><br><br>
+		<table border="0">
+				<tr><td>
+					<b>Leopard safari (Pvt) Ltd </b><br>
+					495/1/2/1,<br>
+					2nd, Queens Road,<br>
+					Jubilie Post,<br>
+					Nugegoda,<br>
+					Sri Lanka.</td>
+					<td> Tel: +94112245456<br> 
+						 &nbsp &nbsp &nbsp +94772245453<br>
+					</td>
+				</tr>
+				</table>
+		</div>
+		<!--Referenced by https://get.foundation-->
+		</div>
+		<div class="social-footer-icons">
+		<ul class="menu simple">
+		<li><a href="https://www.facebook.com/" class="fa fa-facebook" aria-hidden="true"></i></a></li>
+		<li><a href="https://www.instagram.com/?hl=en" class="fa fa-instagram" aria-hidden="true"></i></a></li>
+		<li><a href="https://www.pinterest.com/" class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+		<li><a href="https://twitter.com/?lang=en" class="fa fa-twitter" aria-hidden="true"></i></a></li>
+		</ul>
+		</div>
+	</footer>
+<!--Footer ends-->
+</body>
+</html>
