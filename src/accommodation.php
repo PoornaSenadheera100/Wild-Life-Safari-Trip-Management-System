a7e03e51c148c23d845d5ac50479913cb44d2d2d
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2022-08-21 07:54:10 +0530

    Created and modified accommodation.php file

---
+++ src/accommodation.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Accommodation</title>
+	<script type = "text/javascript" src = "js/script.js"></script>
+	<link rel = "stylesheet" href = "styles/styles.css">
+	<link href='https://fonts.googleapis.com/css?family=Armata' rel='stylesheet'>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+	<ul class ='menu'>
+	  <li class ='menu'><a href="Homepage.php">Home</a></li>
+	  <li class ='dropdown'><a href="tour.php" class="dropbtn">Wildlife Tours</a>
+			<div class="dropdown-content">
+			  <a href="Leopard safari.php">Leopard safari</a>
+			  <a href="Elephant safari.php">Elephant safari</a>
+			  <a href="Photography tour.php">Photography tour</a>
+			  <a href="Bird watching tour.php">Bird watching tour</a>
+			</div></li>
+	  <li class ='menu'><a href="accommodation.php">Accommodation</a></li>
+	  <li class ='menu'><a href="about us.php">About us</a></li>
+	  <li class ='menu'><a href="contact us.php">Contact us</a></li>
+	</ul>
+	
+	<center><img src = "images/logo.png" width = "22%"></center>
+
+	<img src = "images/accommodation.jpg" width = "100%">
+	<br><br><br>
+	<h1><center>ACCOMMODATION</center></h1>
+	
+	<div style ='margin: 20px 50px 100px 50px;'>
+		<p>Safaris are crucial, but where you stay afterward to relax and unwind is just as important, since it adds to your wildlife vacation experience. This can be difficult with so many options available.</p>
+		<p>This is where our experience comes into play. Before venturing back out into the wild, we'll find the greatest wildlife accommodation in Sri Lanka for you to unwind, relax, or even hibernate in.</p>
+		<p>We propose that you stay at one of our selected Tented Safari Camps, Wildlife Lodges, or Hotels in each of Sri Lanka's safari locations, all of which are designed to enhance your wildlife safari experience. Some of our opulent wildlife hotels will make you feel as if you've never left the wild. However, because we are sensitive to our client's demands, we have something for everyone, in both the luxury and affordable categories.</p>
+		<p>Our wildlife lodging is first-rate in terms of both quality and location. You will receive five-star treatment while immersed in Sri Lanka's wildlife - unlike anywhere else; every detail will be scrutinized and addressed. With great wildlife accommodation resources, we are prepared to meet a wide range of individual needs. So tell us what you like and don't like so we can better serve you - we're looking forward to hearing from you.</p>
+
+	<br/>
+	
+	<hr size = "5" color = "black">
+	
+	<h2><center>Accommodation Yala National Park - Sri Lanka</center></h2>
+	
+	<img src = "images/yala/pic0.jpg" width = "100%"><br/>
+	<img src = "images/yala/pic1.jpg" width = "24.5%">
+	<img src = "images/yala/pic2.jpg" width = "24.5%">
+	<img src = "images/yala/pic3.jpg" width = "24.5%">
+	<img src = "images/yala/pic4.jpeg" width = "24.5%">
+	<br/><br/>
+	
+	<table border = "10" cellspacing = "0" class = "accommodationtable">
+		<tr bgcolor = #AED9A0>
+			<th class = "campdescription">MAHOORA ELITE CAMPS</th>
+			<td class = "campdescription">This has a luxurious feel to it, with extra space for visitors that seek comfort during their stay. A bedroom, sitting space, and en-suite bathroom are all included in the large tent. Just outside the tent is a tiny veranda space. The entire tent is carpeted from floor to ceiling, including the washroom. The windows are draped with lovely drapes.</td>
+		</tr>
+		<tr bgcolor = #D9A0A0>
+			<th class = "campdescription">BIG GAME CAMP</th>
+			<td class = "campdescription">When researching Yala safari lodging alternatives, it's critical to pick one that cuts down on travel time so you may spend as much time as possible on safari drives. Big Game Safari camps are located outside the park in a semi-wilderness setting right behind the Nimalawa sanctuary, about a 20-minute drive from the entrance.</td>
+		</tr>
+	</table>
+	<br/>
+	
+	<button class = "btninquire"><a href = "contact us.php"> INQUIRE NOW!</a></button>
+	<br/><br/><br/><br/><br/>
+	
+	<h2><center>Accommodation Udawalawe National Park - Sri Lanka</center></h2>
+
+	<img src = "images/udawalawe/pic0.jpg" width = "100%"><br/>
+	<img src = "images/udawalawe/pic1.jpg" width = "24.5%">
+	<img src = "images/udawalawe/pic2.jpg" width = "24.5%">
+	<img src = "images/udawalawe/pic3.png" width = "24.5%">
+	<img src = "images/udawalawe/pic4.jpg" width = "24.5%">
+	<br/><br/>
+	
+	<table border = "10" cellspacing = "0" class = "accommodationtable">
+	<tr bgcolor = #AED9A0>
+	<th class = "campdescription">MAHOORA ELITE CAMPS</th>
+	<td class = "campdescription">Guests who are looking for a relaxing experience in the natural world prefer this. The tent has a bedroom and an en-suite bathroom, as well as a private veranda/porch on the exterior.</td>
+	</tr>
+	<tr bgcolor = #D9A0A0>
+	<th class = "campdescription">BIG GAME CAMP</th>
+	<td class = "campdescription">Big Game Camp Udawalawe, that is located on the eastern border of Udawalawe National Park and views a lake, provides tented accommodation with modern conveniences such as a private bathroom. The park's main entrance is 30 minutes away, and the Elephant transport home is 45 minutes away. Each fan-cooled tent has elevated beds with mattresses, pillows, and bed linens, as well as a separate, partitioned bathroom with shower and toilet. Towels are given, as well as complimentary bottled water for meals. Safaris, airport transportation, and laundry services are all available for an additional fee.</td>
+	</tr>
+	</table>
+	<br/>
+	
+	<button class = "btninquire"><a href = "contact us.php"> INQUIRE NOW!</a></button>
+	<br/><br/><br/><br/><br/>
+	
+	<h2><center>Accommodation Wilpattu National Park - Sri Lanka</center></h2>
+
+	<img src = "images/wilpattu/pic0.jpg" width = "100%"><br/>
+	<img src = "images/wilpattu/pic1.jpg" width = "24.5%">
+	<img src = "images/wilpattu/pic2.jpg" width = "24.5%">
+	<img src = "images/wilpattu/pic3.jpg" width = "24.5%">
+	<img src = "images/wilpattu/pic4.jpg" width = "24.5%">
+	<br/><br/>
+	
+	<table border = "10" cellspacing = "0" class = "accommodationtable">
+	<tr bgcolor = #AED9A0>
+	<th class = "campdescription">MAHOORA ELITE CAMPS</th>
+	<td class = "campdescription">This is a luxurious setting that provides additional space for those that seek comfort during their stay. The tent has a bedroom, sitting space, and en-suite bathroom. Just outside the tent, there's a tiny veranda space. The tent is carpeted from floor to ceiling, including the washroom. Attractive curtains are draped across the windows.</td>
+	</tr>
+	<tr bgcolor = #D9A0A0>
+	<th class = "campdescription">BIG GAME CAMP</th>
+	<td class = "campdescription">Big Game Camp Wilpattu is located on the outskirts of Wilpattu National Park and offers roomy tents with an attached partitioned bathroom. The camp-site, which is surrounded by wildlife and nature, is located 7 kilometers from the park's entrance and 500 meters from the park's boundary. Each tent has a raised bed with mattress, cushions, bed linen, and a towel that can be zipped. For meals, there is also a seating room, a desk, and free bottled water. There is a shower and a toilet in the private bathroom. Safaris, airport transportation, and laundry services are available for an additional fee.</td>
+	</tr>
+	</table>
+	<br/>
+	
+	<button class = "btninquire"><a href = "contact us.php"> INQUIRE NOW!</a></button>
+	<br/><br/><br/><br/><br/>
+	
+	<h2><center>Accommodation Sinharaja Rainforest - Sri Lanka</center></h2>
+
+	<img src = "images/sinharaja/pic0.jpg" width = "100%"><br/>
+	<img src = "images/sinharaja/pic1.jpg" width = "24.5%">
+	<img src = "images/sinharaja/pic2.jpg" width = "24.5%">
+	<img src = "images/sinharaja/pic3.jpg" width = "24.5%">
+	<img src = "images/sinharaja/pic4.jpg" width = "24.5%">
+	<br/><br/>
+	
+	<table border = "10" cellspacing = "0" class = "accommodationtable">
+	<tr bgcolor = #AED9A0>
+	<th class = "campdescription">MAHOORA ELITE CAMPS</th>
+	<td class = "campdescription">The Sinharaja Rainforest is the best destination in Sri Lanka for bird enthusiasts. Because of the great degree of endemism observed in the plant, avian, and reptilian species that calls this virgin rainforest home, it has been classified as a UNESCO World Heritage Site and Biosphere Reserve.</td>
+	</tr>
+	<tr bgcolor = #D9A0A0>
+	<th class = "campdescription">BIG GAME CAMP</th>
+	<td class = "campdescription">Explore Sinharaja Rain Forest Tour Camp is located near Deniyaya entrance in the UNESCO site of Sinharaja Forest Reserve and offers a BBQ and mountain views. The hotel provides a shuttle service to and from Mederipitiya Village. All of the rooms are air conditioned, and the tents have a lounging area and a private bathroom with a shower. Bicycles are available for free at this motel, and fishing is popular in the neighborhood. There are barbecue grills and laundry services available. Explore Sinharaja is a 2-hour journey between Galle and Udawalawa, and 112 miles from Bandaranaike International Airport.</td>
+	</tr>
+	</table>
+	<br/>
+	
+	<button class = "btninquire"><a href = "contact us.php"> INQUIRE NOW!</a></button>
+	<br/><br/><br/>
+	</div>
+
+
+
+
+
+
+
+	<footer class="social-footer">
+	<div class="social-footer-left">
+	<div style ='text-align:left;margin: 30px 30px 30px 60px ;'>
+	<img src="images/logo.png" style="width:30%"><br><br>
+	<table border="0">
+			<tr><td>
+				<b>Leopard safari (Pvt) Ltd </b><br>
+				495/1/2/1,<br>
+				2nd, Queens Road,<br>
+				Jubilie Post,<br>
+				Nugegoda,<br>
+				Sri Lanka.</td>
+				<td> Tel: +94112245456<br> 
+					 &nbsp &nbsp &nbsp +94772245453<br>
+				</td>
+			</tr>
+			</table>
+	</div>
+	<!--Referenced by https://get.foundation-->
+	</div>
+	<div class="social-footer-icons">
+	<ul class="menu simple">
+	<li><a href="https://www.facebook.com/" class="fa fa-facebook" aria-hidden="true"></i></a></li>
+	<li><a href="https://www.instagram.com/?hl=en" class="fa fa-instagram" aria-hidden="true"></i></a></li>
+	<li><a href="https://www.pinterest.com/" class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+	<li><a href="https://twitter.com/?lang=en" class="fa fa-twitter" aria-hidden="true"></i></a></li>
+	</ul>
+	</div>
+	</footer>
+	
+
+</body>
+
+</html>
